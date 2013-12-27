a5ea19c9e5d2670cd31754082a0878bed74bba42
Amit Rawat <amraw@amraw-K84L.(none)>
2013-12-27 20:43:02 +0530

    Insertion sort code

---
+++ algorithms/insertion_sort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int *c,key,j,i=0;
+	c=(int *) calloc(100,sizeof(int));
+	printf("#####Enter the number & -1 to exit####\n");
+	scanf("%d",&key);
+	while(key>=0 && i<=100)
+	{
+		j=i-1;
+		while(j>=0 && (key < *(c+j)))
+		{
+			*(c+j+1)=*(c+j);
+			j=j-1;
+		}
+		*(c+j+1)=key;
+		i=i+1;
+		scanf("%d",&key);
+	}
+	printf("Sorted Array:");
+	for(j=0;j<i;j++)
+	printf(" %d ",*(c+j));
+	printf("\n");
+	return 0;
+}
